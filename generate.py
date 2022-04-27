d5c243038ddfba901f4e53d6de5019c14a2293ff
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2022-04-27 19:09:07 -0400

    update readme

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
 import numpy as np
-import sys
+import zipfile
 import json
 import gym
 import textworld.gym
@@ -7,7 +7,7 @@ import textworld
 from tqdm import tqdm
 import os
 from os import listdir
-from os.path import isfile, join
+from os.path import join
 import gc
 
 def generate_game(n_games=100):
@@ -53,20 +53,20 @@ def info2graph(infos):
     return graph
 
 def walk_thr(walks_per_game=10, max_walk=200):
+
+    if not os.path.exists('game_walks'):
+        os.mkdir('game_walks')
+
     walks = []
     games = [join('my_games', f, 'game.ulx') for f in listdir('my_games')]
     count = 0
     length = 0
-    game_count = 0
-    for g in tqdm(games):
-        game_count += 1
-        if game_count in [140, 141, 324]: # problematic games
-            continue
+    for g in (pbar := tqdm(games)):
         if length > 10000:
-            game_walks_file = os.path.join('dataset', 'game_walks_' + str(count) + '.json')
+            game_walks_file = os.path.join('game_walks', 'game_walks_' + str(count) + '.json')
             with open(game_walks_file, 'w') as file:
                 json.dump(walks, file)
-                print('saved!')
+                pbar.set_description("file saved: " + str(count + 1))
             count += 1
             walks = []
             length = 0
@@ -75,15 +75,16 @@ def walk_thr(walks_per_game=10, max_walk=200):
             walk = one_walk_thr(g, max_walk)
             length += len(walk)
             walks.append(walk)
-    game_walks_file = os.path.join('dataset', 'game_walks_' + str(count) + '.json')
+
+    game_walks_file = os.path.join('game_walks', 'game_walks_' + str(count) + '.json')
     with open(game_walks_file, 'w') as file:
         json.dump(walks, file)
-        print('saved!')
+        print('complete!')
     
     
 
 def one_walk_thr(game_dir, max_walk=200):
-    request_infos = textworld.EnvInfos(admissible_commands=True, facts=True, policy_commands=True)
+    request_infos = textworld.EnvInfos(admissible_commands=True, facts=True, policy_commands=True, verbs=True, entities=True)
     env_id = textworld.gym.register_game(game_dir, request_infos)
     env = gym.make(env_id)
     env.reset()
@@ -92,6 +93,7 @@ def one_walk_thr(game_dir, max_walk=200):
     obs = None
     done = False
     infos = None
+    last_valid = False
     for i in range(max_walk):
         state = {}
         if (initial):
@@ -100,10 +102,25 @@ def one_walk_thr(game_dir, max_walk=200):
             initial = False
         else:
             last_state = walk[-1]
-            if i % 5 == 0:
+            if last_valid: # take a look after each policy action
+                prev_action = 'look'
+                last_valid = False
+            if i % 5 == 0: # choose policy action
+                last_valid = True
                 policy = infos['policy_commands']
                 prev_action = policy[0]
-            else:
+            elif i % 5 == 1: # choose an invalid action 
+                verb = infos['verbs']
+                entities = infos['entities']
+                invalid_cmd = None
+                while True:
+                    vidx = np.random.randint(len(verb))
+                    eidx = np.random.randint(len(entities))
+                    invalid_cmd = verb[vidx] + ' ' + entities[eidx]
+                    if invalid_cmd not in last_state['valid_actions']:
+                        break
+                prev_action = invalid_cmd
+            else: # choose a random valid action
                 idx = np.random.randint(len(last_state['valid_actions']))
                 prev_action = last_state['valid_actions'][idx]
             obs, _, done, infos = env.step(prev_action)
@@ -117,9 +134,16 @@ def one_walk_thr(game_dir, max_walk=200):
             break
     return walk
 
+def unzip_games():
+    if not os.path.exists('my_games'):
+        os.mkdir('my_games')
+
+    with zipfile.ZipFile('my_games.zip', 'r') as zip_ref:
+        zip_ref.extractall('my_games')
 
 def main():
     gc.enable()
+    #unzip_games()
     #generate_game(1000)
     walk_thr()
     return 0
