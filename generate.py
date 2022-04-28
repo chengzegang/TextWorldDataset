e56762cf8a697569cb98fd8c2563fe86cb27cf5f
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2022-04-28 13:47:31 -0400

    update game_walks

--- generate.py
+++ generate.py
@@ -9,6 +9,7 @@ import os
 from os import listdir
 from os.path import join
 import gc
+import re
 
 def generate_game(n_games=100):
 
@@ -25,6 +26,16 @@ def generate_game(n_games=100):
 
 def info2graph(infos):
     graph = []
+    items = infos['inventory']
+    if 'nothing' not in items:
+        # translate inventory descriptions into a list of items
+        items = items[17:]
+        items = items.replace('and', ',')
+        items = items.split(',')
+        items = [i.strip()[2:-1] for i in items]
+        # print(items)
+        for i in items:
+            graph.append(['you', 'has', i])
     for f in infos['facts']:
         f = f.serialize()
         sub = None
@@ -45,9 +56,9 @@ def info2graph(infos):
             sub = args[0]['name']
             rel = name
             obj = args[1]['name']
-            graph.append([sub, rel, obj])
             if sub == 'P':
                 sub = 'you'
+            graph.append([sub, rel, obj])
         else:
             print('error: unknown type')
     return graph
@@ -68,12 +79,12 @@ def walk_thr(walks_per_game=10, max_walk=200):
     finished = set(desc_file['finished'])
 
     walks = []
-    broken_game = []
-    games = [(f, join('my_games', f, 'game.ulx')) for f in listdir('my_games') if f not in finished and f not in broken_game]
+    broken_game = ['1196862928', '517879001', '2494778704', '3864721294', '808678209'] # program freezes when running on these
+    games = [(f, join('my_games', f, 'game.ulx')) for f in listdir('my_games') if f not in finished and f not in broken_game and os.path.isdir('my_games/' + f)]
     count = 0
     length = 0
     for id, g in (pbar := tqdm(games)):
-        pbar.set_description("file saved: " + str(count) + "; current file: " + id)
+        pbar.set_description("file saved: " + str(count) + "; current game: " + id)
         for i in range(walks_per_game):
             walk = one_walk_thr(g, max_walk)
             length += len(walk)
@@ -81,7 +92,7 @@ def walk_thr(walks_per_game=10, max_walk=200):
         game_walks_file = os.path.join('game_walks', 'game_walks_' + id + '.json')
         with open(game_walks_file, 'w') as file:
             json.dump(walks, file)
-            pbar.set_description("file saved: " + str(count + 1) + "; current file: " + id)
+            pbar.set_description("file saved: " + str(count + 1) + "; current game: " + id)
         count += 1
         walks = []
         length = 0
@@ -100,7 +111,7 @@ def walk_thr(walks_per_game=10, max_walk=200):
     
 
 def one_walk_thr(game_dir, max_walk=200):
-    request_infos = textworld.EnvInfos(admissible_commands=True, facts=True, policy_commands=True, verbs=True, entities=True)
+    request_infos = textworld.EnvInfos(admissible_commands=True, facts=True, policy_commands=True, verbs=True, entities=True, inventory=True, fail_facts=True)
     env_id = textworld.gym.register_game(game_dir, request_infos)
     env = gym.make(env_id)
     env.reset()
@@ -121,21 +132,25 @@ def one_walk_thr(game_dir, max_walk=200):
             if last_valid: # take a look after each policy action
                 prev_action = 'look'
                 last_valid = False
-            if i % 5 == 0: # choose policy action
+            if i % 10 == 0: # choose policy action
                 last_valid = True
                 policy = infos['policy_commands']
                 prev_action = policy[0]
-            elif i % 5 == 1: # choose an invalid action 
+            elif i % 10 == 1: # choose an invalid action 
                 verb = infos['verbs']
                 entities = infos['entities']
                 invalid_cmd = None
+                times = 0
                 while True:
+                    times += 1
                     vidx = np.random.randint(len(verb))
                     eidx = np.random.randint(len(entities))
                     invalid_cmd = verb[vidx] + ' ' + entities[eidx]
-                    if invalid_cmd not in last_state['valid_actions']:
+                    if invalid_cmd not in last_state['valid_actions'] or times > 1000:
                         break
                 prev_action = invalid_cmd
+            elif i % 10 == 2: # take a look at player's inventory
+                prev_action = 'inventory'
             else: # choose a random valid action
                 idx = np.random.randint(len(last_state['valid_actions']))
                 prev_action = last_state['valid_actions'][idx]
